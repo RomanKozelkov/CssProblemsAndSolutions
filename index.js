bae9362f89bd625352e81b6d59f6ce6afe98f409
Roman Kozelkov <kozelkov.roman@gmail.com>
2017-11-12 13:03:07 +0300

    image cover solutions

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ var nodeStatic = require('node-static');
 
 var file = new nodeStatic.Server('./public');
 
-console.log('req');
-
 require('http').createServer(function (request, response) {
-	console.log('req');
 	request.addListener('end', function () {
-		console.log('el listener');
 		file.serve(request, response);
 	}).resume();
 }).listen(8033);
